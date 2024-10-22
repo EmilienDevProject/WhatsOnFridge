f327dae964694ccc1bf0451a3f03d2369a61c804
EmilienDevProject <emilien.devproject@gmail.com>
2024-10-22 19:18:34 +0200

    firstCommitAddHomePageFile

---
+++ WhatsOnFridge/HomePage/HomeInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  HomeInteractor.swift
+//  WhatsOnFridge
+//
+//  Created by Emilien on 22/10/2024.
+//
+
+import Foundation
---
+++ WhatsOnFridge/HomePage/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  WhatsOnFridge
+//
+//  Created by Emilien on 22/10/2024.
+//
+
+import Foundation
