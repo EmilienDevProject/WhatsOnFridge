415176b902eb704419d69f01d6245c88d7cab04d
EmilienDevProject <emilien.devproject@gmail.com>
2024-10-22 18:59:16 +0200

    Initial commit

---
+++ WhatsOnFridge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsOnFridge
+//
+//  Created by Emilien on 22/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
